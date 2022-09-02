386fa76172c7c121ef8310318f553970e25d62ee
YONGHUN <kooaas@daum.net>
2022-09-02 15:53:38 +0900

    renewal splash

--- app/src/main/java/com/example/lifelonglearningapp/MainActivity.kt
+++ app/src/main/java/com/example/lifelonglearningapp/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.lifelonglearningapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.LifelongLearningApp)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
