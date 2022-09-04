87a3ae7e47bc2d8accfaa061638588ff7c1cbf4d
YONGHUN <kooaas@daum.net>
2022-09-04 23:54:11 +0900

    renewal splash 2

--- app/src/main/java/com/example/lifelonglearningapp/MainActivity.kt
+++ app/src/main/java/com/example/lifelonglearningapp/MainActivity.kt
@@ -1,7 +1,16 @@
 package com.example.lifelonglearningapp
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.View
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +18,8 @@ class MainActivity : AppCompatActivity() {
         setTheme(R.style.LifelongLearningApp)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
 }
