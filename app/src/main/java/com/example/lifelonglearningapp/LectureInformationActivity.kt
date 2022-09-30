05bb7079d1fd7b22954d3aa7c30682b36b6f2147
YONGHUN <kooaas@daum.net>
2022-09-30 23:06:36 +0900

    lectureinformation 설정

---
+++ app/src/main/java/com/example/lifelonglearningapp/LectureInformationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lifelonglearningapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LectureInformationActivity : AppCompatActivity()  {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lectureinformation)
+    }
+}
