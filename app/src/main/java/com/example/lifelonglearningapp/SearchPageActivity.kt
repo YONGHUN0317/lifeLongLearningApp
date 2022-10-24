363a2122877b21d87b957a95610ad2fcad8a4147
YONGHUN <kooaas@daum.net>
2022-10-25 00:04:31 +0900

    searchview 해결

--- app/src/main/java/com/example/lifelonglearningapp/SearchPageActivity.kt
+++ app/src/main/java/com/example/lifelonglearningapp/SearchPageActivity.kt
@@ -1,12 +1,22 @@
 package com.example.lifelonglearningapp
 
+import android.graphics.Color
+import android.graphics.Color.BLACK
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.lifelonglearningapp.databinding.ActivityAftersearchBinding
+import com.example.lifelonglearningapp.databinding.ActivityMainBinding
+
 
 class SearchPageActivity : AppCompatActivity()  {
 
+    private lateinit var binding:  ActivityAftersearchBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_aftersearch)
+        binding = ActivityAftersearchBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
     }
 }
